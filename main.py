58ea0b85725b7bcf440e08ec30cd09d290d82a6a
Skulsta <sti.sku@gmail.com>
2018-04-24 15:06:42 +0200

    he

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ from sklearn.feature_extraction import DictVectorizer
 
 import pandas as pd
 
-dataset = pd.read_table('seeds_dataset.txt')
+with open('seeds_dataset.txt') as f:
+    table = pd.read_table(f, sep='-', index_col=0, header=None, names=['A', 'B', 'C', 'D', 'E', 'F', ], delimiter='\t')
 
-print(dataset)
+# dataset = pd.read_table('seeds_dataset.txt')
 
-print(type(dataset))
-
-dataset = dataset.values
-
-vec = DictVectorizer()
-
-print(type(dataset))
+print(table)
