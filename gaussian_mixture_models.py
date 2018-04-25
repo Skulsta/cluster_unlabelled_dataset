4d5df906cdf2b6f733725230f77ddfc638bde383
Skulsta <sti.sku@gmail.com>
2018-04-25 14:24:40 +0200

    added gmm

--- gaussian_mixture_models.py
+++ gaussian_mixture_models.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from sklearn.mixture import GaussianMixture
 
 colnames = ['area', 'perimeter', 'compactness', 'lenghtOfKernel', 'widthOfKernel', 'assymetryCoefficient',
         'lenghtOfKernelGrove', 'classifier']
 
-data = pd.read_table("seeds_dataset.txt", index_col=False, names=colnames, header=None, delimiter='\t\t|\t')
+data = pd.read_table("seeds_dataset.txt", index_col=False, names=colnames, header=None, delimiter='\s+')
 
 numpy_array = data.values
 label = numpy_array[:, 7]   # The last culomn. The class label.
@@ -11,3 +12,6 @@ features = numpy_array[:, :7]   # From index 0 to 6. 7 feature types.
 
 print(features)
 print(label)
+
+gmm = GaussianMixture(n_components=3).fit(features)
+result_labels = gmm.predict(features)
