7c57f9c70686f88bafbaeb6a12dfa7a8ff9b7bf4
Magnus-V <magnves@gmail.com>
2018-04-30 10:33:57 +0200

    heyhey

---
+++ plot_kmeans_seeds.py
@@ -0,0 +1,31 @@
+"""
+Our demonstration of the kmeans algorithm used on the seeds datasets provided. Info284 - Spring 2018.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+colnames = ['area', 'perimeter', 'compactness', 'lenghtOfKernel', 'widthOfKernel', 'assymetryCoefficient',
+            'lengthOfKernelGrove', 'classifier']
+
+data = pd.read_table("seeds_dataset.txt", index_col=False, names=colnames, header=None, delimiter='\s+')
+
+numpy_array = data.values
+
+y = numpy_array[:, 7]  # The last column. The class label. Not used in our unsupervised learning.
+X = numpy_array[:, :7]  # From index 0 to 6. 7 feature types.
+
+
+pca = PCA(n_components=2, svd_solver='full')
+pca.fit(X);
+X = pca.transform(X);
+
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(X)
+print(kmeans.labels_)
+plt.scatter(X[:, 0], X[:, 1], c=kmeans.labels_)
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=70, c='blue')
+plt.show()
